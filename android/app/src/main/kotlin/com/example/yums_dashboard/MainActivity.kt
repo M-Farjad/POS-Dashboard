bb024e6c5abf614c963d99d959e1f7cf6009bf8c
M-Farjad <2021se56@student.uet.edu.pk>
2023-03-05 20:02:35 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/yums_dashboard/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yums_dashboard
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'constants.dart';
+import 'screens/main/main_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Admin Panel',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: bgColor,
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+            .apply(bodyColor: Colors.white),
+        canvasColor: secondaryColor,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
