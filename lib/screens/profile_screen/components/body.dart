8d157ab255a5df2ac385a01d53ce36924ccf0b74
M-Farjad <2021se56@student.uet.edu.pk>
2023-05-01 22:48:51 +0500

    Profile Text Fields

--- lib/screens/profile_screen/components/body.dart
+++ lib/screens/profile_screen/components/body.dart
@@ -3,11 +3,20 @@ import 'package:flutter/material.dart';
 
 import '../../../main.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   Body({super.key});
+
+  @override
+  State<Body> createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
+    final _nameController = TextEditingController();
+    final emailController = TextEditingController();
     return Form(
       key: _formKey,
       child: SingleChildScrollView(
@@ -16,43 +25,56 @@ class Body extends StatelessWidget {
           children: [
             //for adding some space
             SizedBox(width: mq.width, height: mq.height * 0.03),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: mq.width * .04),
-              child: TextFormField(
-                onSaved: (val) {},
-                validator: (val) =>
-                    val != null && val.isNotEmpty ? null : '*Required',
-                initialValue: 'Salman Pagal',
-                decoration: InputDecoration(
-                  hintText: 'e.g Muhammad Farjad',
-                  label: const Text('Name'),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  prefixIcon: Icon(CupertinoIcons.person,
-                      color: Theme.of(context).primaryColor),
-                ),
-              ),
+            CustomTextField(
+              title: 'Name',
+              hintText: 'e.g Salman Naeem',
+              iconData: CupertinoIcons.person,
+              controller: _nameController,
             ),
             SizedBox(height: mq.height * 0.02),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: mq.width * .04),
-              child: TextFormField(
-                onSaved: (val) {},
-                validator: (val) =>
-                    val != null && val.isNotEmpty ? null : '*Required',
-                initialValue: 'abc@example.com',
-                decoration: InputDecoration(
-                  hintText: 'e.g abc@example.com',
-                  label: const Text('Email'),
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  prefixIcon: Icon(Icons.email_rounded,
-                      color: Theme.of(context).primaryColor),
-                ),
-              ),
+            CustomTextField(
+              hintText: 'e.g abc@example.com',
+              title: 'Email',
+              iconData: Icons.email_rounded,
+              controller: _nameController,
             ),
             SizedBox(height: mq.height * 0.05),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  CustomTextField({
+    super.key,
+    required this.title,
+    // this.initialText,
+    required this.controller,
+    required this.iconData,
+    required this.hintText,
+  });
+  final String title;
+  final String hintText;
+
+  // final String? initialText;
+  TextEditingController? controller;
+  final IconData iconData;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: mq.width * .04),
+      child: TextFormField(
+        // controller: controller,
+        onSaved: (val) {},
+        validator: (val) => val != null && val.isNotEmpty ? null : '*Required',
+        initialValue: controller!.text, //?? Yaha error hn
+        decoration: InputDecoration(
+          hintText: hintText,
+          label: Text(title),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+          prefixIcon: Icon(iconData, color: Theme.of(context).primaryColor),
         ),
       ),
     );
